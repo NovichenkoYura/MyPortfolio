48f875404ae2c4703fcaa9b7524babe228954045
NovichenkoYura <novichenko.y@gmail.com>
2022-11-09 17:37:47 +0200

    list

--- template.jsx
+++ template.jsx
@@ -1,13 +1,123 @@
 <h2>React/Redux</h2>
 <ul>
-    <li><a href="">Self-guided tours</a></li>
-    <li><a href="">Comments form</a></li>
-    <li><a href="">Note editor</a></li>
+    <li><a href="https://www.youtube.com/watch?v=ZJ8lNTee7z8">Self-guided tours</a></li>
+    <li><a href="https://www.youtube.com/watch?v=-DaRnFj7qEA">Comments form</a></li>
+    <li><a href="https://www.youtube.com/watch?v=xtuxj5Lr7D8">Note editor</a></li>
 </ul>
 
 <h2>JS</h2>
 <ul>
-    <li><a href="">Movies library</a></li>
+    <li><a href="https://lena-stukalo.github.io/command-project-js/">Movies library</a></li>
     <li><a href="">Todo list</a></li>
-    <li><a href="">Search app</a></li>
+    <li><a href="https://novichenkoyura.github.io/goit-js-hw-11/">Searching of images</a></li>
+    <li><a href="https://novichenkoyura.github.io/goit-js-hw-10/">Searching of countrie's info</a></li>
+    <li><a href="https://novichenkoyura.github.io/goit-js-hw-09/01-color-switcher.html">Color switcher</a></li>
+    <li><a href="https://novichenkoyura.github.io/goit-js-hw-09/02-timer.html">Countdown timer</a></li>
+  
+    
 </ul>
+
+<h2>HTML/CSS</h2>
+<ul>
+    <li><a href="https://lebraise.github.io/project-1-homemade-icescream/">Sweets shop site</a></li>
+    <li><a href="https://novichenkoyura.github.io/goit-markup-hw-08/">Web studio site</a></li>    
+</ul>
+
+
+
+
+
+
+
+
+<h2>React/Redux</h2>
+
+<h3><a href="https://www.youtube.com/watch?v=ZJ8lNTee7z8">Self-guided tours</a></h3>
+<p><a href="https://github.com/NovichenkoYura/self-guided-tours/tree/dev/my-apps">see code</a></p>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero incidunt tempora quibusdam ipsam rerum repudiandae, deserunt sapiente voluptas omnis dolor quaerat cupiditate quas ut vel iusto suscipit nulla dolorum consectetur.
+Tempore, ut dolor, animi odit voluptatibus quas eius, nulla quo sint qui earum inventore numquam itaque rerum. Similique sapiente ducimus, quae atque expedita fugiat, doloremque nisi eaque eos saepe provident!
+Culpa nulla quam blanditiis officia ea obcaecati, rerum minima temporibus eligendi nisi consequuntur perferendis delectus minus eveniet tenetur debitis totam dolorum suscipit cumque ab sed! Enim totam maxime nam at.
+</p>
+
+<h4>Libs & technologies</h4>
+<ul></ul>
+<li>React</li>
+<li>React-redux</li>
+<li>TypeScript</li>
+<li>Axios, createAsyncThunk</li>
+<li>ReactRouterDom v.6</li>
+<li>Formik, Yup</li>
+<li>JSON server</li>
+<li>useState, useEffect, useDispatch, useSelector</li>
+
+
+<h3><a href="https://www.youtube.com/watch?v=-DaRnFj7qEA">Comments form</a></h3>
+<p><a href="https://github.com/NovichenkoYura/comments-form/tree/dev">see code</a></p>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero incidunt tempora quibusdam ipsam rerum repudiandae, deserunt sapiente voluptas omnis dolor quaerat cupiditate quas ut vel iusto suscipit nulla dolorum consectetur.
+Tempore, ut dolor, animi odit voluptatibus quas eius, nulla quo sint qui earum inventore numquam itaque rerum. Similique sapiente ducimus, quae atque expedita fugiat, doloremque nisi eaque eos saepe provident!
+Culpa nulla quam blanditiis officia ea obcaecati, rerum minima temporibus eligendi nisi consequuntur perferendis delectus minus eveniet tenetur debitis totam dolorum suscipit cumque ab sed! Enim totam maxime nam at.
+</p>
+
+<h4>Libs & technologies</h4>
+<ul></ul>
+<li>React</li>
+<li>React-redux</li>
+<li>TypeScript</li>
+<li>Axios, createAsyncThunk</li>
+<li>Formik, Yup</li>
+<li>JSON server</li>
+<li>useState, useEffect, useDispatch, useSelector</li>
+
+
+<h3><a href="https://www.youtube.com/watch?v=xtuxj5Lr7D8">Note editor</a></h3>
+<p><a href="https://github.com/NovichenkoYura/noteeditor-redux-/tree/dev">see code</a></p>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero incidunt tempora quibusdam ipsam rerum repudiandae, deserunt sapiente voluptas omnis dolor quaerat cupiditate quas ut vel iusto suscipit nulla dolorum consectetur.
+Tempore, ut dolor, animi odit voluptatibus quas eius, nulla quo sint qui earum inventore numquam itaque rerum. Similique sapiente ducimus, quae atque expedita fugiat, doloremque nisi eaque eos saepe provident!
+Culpa nulla quam blanditiis officia ea obcaecati, rerum minima temporibus eligendi nisi consequuntur perferendis delectus minus eveniet tenetur debitis totam dolorum suscipit cumque ab sed! Enim totam maxime nam at.
+</p>
+
+<h4>Libs & technologies</h4>
+<ul></ul>
+<li>React</li>
+<li>React-redux</li>
+<li>TypeScript</li>
+<li>Axios, createAsyncThunk</li>
+<li>Formik, Yup</li>
+<li>JSON server</li>
+<li>useState, useEffect, useDispatch, useSelector</li>
+
+
+<h2>JS</h2>
+
+<h3><a href="https://lena-stukalo.github.io/command-project-js/">Movies library</a></h3>
+<p><a href="https://github.com/Lena-Stukalo/command-project-js">see code</a></p>
+
+<h3><a href="">Todo list</a></h3>
+<p><a href="">see code</a></p>
+
+<h3><a href="https://novichenkoyura.github.io/goit-js-hw-11/">Searching of images</a></h3>
+<p><a href="https://github.com/NovichenkoYura/goit-js-hw-11">see code</a></p>
+
+<h3><a href="https://novichenkoyura.github.io/goit-js-hw-10/">Searching of Countrie's info</a></h3>
+<p><a href="https://github.com/NovichenkoYura/goit-js-hw-10">see code</a></p>
+
+<h3><a href="https://novichenkoyura.github.io/goit-js-hw-09/01-color-switcher.html">Color switcher</a></h3>
+<p><a href="https://github.com/NovichenkoYura/goit-js-hw-09/tree/main/src">see code</a></p>
+
+<h3><a href="https://novichenkoyura.github.io/goit-js-hw-09/02-timer.html">Countdown timer</a></h3>
+<p><a href="https://github.com/NovichenkoYura/goit-js-hw-09/tree/main/src">see code</a></p>
+
+
+<h2>HTML/CSS</h2>
+
+<h3><a href="https://lebraise.github.io/project-1-homemade-icescream/">Sweets shop site</a></h3>
+<p><a href="https://github.com/Lebraise/project-1-homemade-icescream">see code</a></p>
+
+<h3><a href="https://novichenkoyura.github.io/goit-markup-hw-08/">Web studio site</a></h3>
+<p><a href="https://github.com/NovichenkoYura/goit-markup-hw-08">see code</a></p>
+
+
+
